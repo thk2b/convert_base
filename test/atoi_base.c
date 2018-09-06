b4db3a7438d91af99c1477f65783b64268671efb
Theo <theokobb@gmail.com>
2018-09-06 11:47:51 -0400

    refactor tests after ctest update

--- test/atoi_base.c
+++ test/atoi_base.c
@@ -5,31 +5,35 @@
 
 void	test_atoi_base(void)
 {
-	test_start("atoi_base");
+	test_title("atoi_base");
 
-	test_eq_int(atoi_base("1", 10), 1, "1 base 10");
-	test_eq_int(atoi_base("-1", 10), -1, "-1 base 10");
-	test_eq_int(atoi_base("1", 2), 1, "1 base 2");
-	test_eq_int(atoi_base("9210", 10), 9210, "9210 base 10");
-	test_eq_int(atoi_base("2147483647", 10), INT_MAX, "INT_MIN base 10");
-	test_eq_int(atoi_base("-2147483648", 10), INT_MIN, "INT_MIN base 10");
+	test_name(1, "base 10");
+	test_eq_int(atoi_base("1", 10), 1, "1");
+	test_eq_int(atoi_base("-1", 10), -1, "-1");
+	test_eq_int(atoi_base("1", 2), 1, "1");
+	test_eq_int(atoi_base("9210", 10), 9210, "9210");
+	test_eq_int(atoi_base("2147483647", 10), INT_MAX, "INT_MIN");
+	test_eq_int(atoi_base("-2147483648", 10), INT_MIN, "INT_MIN");
 
-	test_eq_int(atoi_base("1", 2), 1, "3 base 2");
-	test_eq_int(atoi_base("1111111111111111111111111111111", 2), INT_MAX, "INT_MAX base 2");
-	test_eq_int(atoi_base("-1111111111111111111111111111111", 2), INT_MIN+1, "INT_MIN+1 base 2");
-	test_eq_int(atoi_base("10000000000000000000000000000000", 2), INT_MIN, "INT_MIN base 2");
-	test_eq_int(atoi_base("1010", 2), 10, "10 base 2");
+	test_name(1, "base 2");
+	test_eq_int(atoi_base("1", 2), 1, "3");
+	test_eq_int(atoi_base("1111111111111111111111111111111", 2), INT_MAX, "INT_MAX");
+	test_eq_int(atoi_base("-1111111111111111111111111111111", 2), INT_MIN+1, "INT_MIN+1");
+	test_eq_int(atoi_base("10000000000000000000000000000000", 2), INT_MIN, "INT_MIN");
+	test_eq_int(atoi_base("1010", 2), 10, "10");
 
-	test_eq_int(atoi_base("2f", 16), 47, "47 base 16");
-	test_eq_int(atoi_base("2F", 16), 47, "47 base 16 uppercase");
-	test_eq_int(atoi_base("-2F", 16), -47, "-47 base 16");
-	test_eq_int(atoi_base("7FFFFFFF", 16), INT_MAX, "INT_MAX base 16");
-	test_eq_int(atoi_base("-7FFFFFFF", 16), INT_MIN+1, "INT_MIN+1 base 16");
-	test_eq_int(atoi_base("7DE86A9F", 16), 2112383647, "2112383647 base 16");
+	test_name(1, "base 16");
+	test_eq_int(atoi_base("2f", 16), 47, "47");
+	test_eq_int(atoi_base("2F", 16), 47, "47");
+	test_eq_int(atoi_base("-2F", 16), -47, "-47");
+	test_eq_int(atoi_base("7FFFFFFF", 16), INT_MAX, "INT_MAX");
+	test_eq_int(atoi_base("-7FFFFFFF", 16), INT_MIN+1, "INT_MIN+1");
+	test_eq_int(atoi_base("7DE86A9F", 16), 2112383647, "2112383647");
 	
 	test_eq_int(atoi_base("   -1", 10), -1, "spaces before number");
 	test_eq_int(atoi_base("   +1", 10), 1, "spaces before number with +");
 
+	test_name(1, "invalid input");
 	test_eq_int(atoi_base("122+4", 10), 122, "invalid symbol");
 	test_eq_int(atoi_base("12229a12", 10), 12229, "symbol beyond base");
 	test_eq_int(atoi_base("01201", 2), 1, "symbol beyond base");
--- test/itoa_base.c
+++ test/itoa_base.c
@@ -4,21 +4,24 @@
 
 void		test_itoa_base(void)
 {
-	test_start("atoi_base");
+	test_title("atoi_base");
 	
-	test_eq_str(itoa_base(10, 10), "10", "10 base 10");
-	test_eq_str(itoa_base(123, 10), "123", "123 base 10");
-	test_eq_str(itoa_base(-10, 10), "-10", "-10 base 10");
-	test_eq_str(itoa_base(INT_MAX / 2, 10), "1073741823", "INT_MAX / 2 base 10");
-	test_eq_str(itoa_base(INT_MAX, 10), "2147483647", "INT_MAX base 10");
-	test_eq_str(itoa_base(INT_MIN, 10), "-2147483648", "INT_MIN base 10");
-	test_eq_str(itoa_base(INT_MIN / 2, 10), "-1073741824", "INT_MIN / 2 base 10");
+	test_name(1, "base 10");
+	test_eq_str(itoa_base(10, 10), "10", "10");
+	test_eq_str(itoa_base(123, 10), "123", "123");
+	test_eq_str(itoa_base(-10, 10), "-10", "-10");
+	test_eq_str(itoa_base(INT_MAX / 2, 10), "1073741823", "INT_MAX / 2");
+	test_eq_str(itoa_base(INT_MAX, 10), "2147483647", "INT_MAX");
+	test_eq_str(itoa_base(INT_MIN, 10), "-2147483648", "INT_MIN");
+	test_eq_str(itoa_base(INT_MIN / 2, 10), "-1073741824", "INT_MIN / 2");
 
-	test_eq_str(itoa_base(10, 2), "1010", "-10 base 2");
-	test_eq_str(itoa_base(INT_MAX, 2), "1111111111111111111111111111111", "INT_MAX base 2");
+	test_name(1, "base 2");
+	test_eq_str(itoa_base(10, 2), "1010", "-10");
+	test_eq_str(itoa_base(INT_MAX, 2), "1111111111111111111111111111111", "INT_MAX");
 
-	test_eq_str(itoa_base(47, 16), "2F", "47 base 16");
-	test_eq_str(itoa_base(-47, 16), "-2F", "-47 base 16");
+	test_name(1, "base 16");
+	test_eq_str(itoa_base(47, 16), "2F", "47");
+	test_eq_str(itoa_base(-47, 16), "-2F", "-47");
 
 	test_end();
 }
